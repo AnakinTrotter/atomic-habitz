e51e5fe0354003178808d0ad6b3f8f313b091e50
Anakin Trotter <anakin.trotter@gmail.com>
2022-04-16 14:02:42 -0700

    Updated home layout

--- components/HomeHeaderView.js
+++ components/HomeHeaderView.js
@@ -2,20 +2,20 @@ import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import HomeDateText from './HomeDateText';
 
+const HomeHeaderView = () => {
+    return (
+        <View>
+            <Text style={styles.titleText}>Your Habits</Text>
+            <HomeDateText />
+        </View>
+    );
+}
+
 const styles = StyleSheet.create({
     titleText: {
         fontSize: 32,
         fontWeight: "bold"
     }
 });
-
-const HomeHeaderView = () => {
-    return (
-        <View>
-            <Text style={ styles.titleText }>Your Habits</Text>
-            <HomeDateText />
-        </View>
-    );
-}
 
 export default HomeHeaderView;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,30 +1,37 @@
 import {
   StyleSheet,
-  Text,
+  SafeAreaView,
   View,
   TextInput,
   TouchableOpacity,
 } from "react-native";
 
 import HomeHeaderView from '../components/HomeHeaderView.js'
+import { COLORS } from '../constants/colors'
 
 function HomeScreen(props) {
   // the HomeHeaderView is for testing rn
   return (
-    <View style={styles.container}>
-      <HomeHeaderView />
-      <Text>Hi this is the home screen</Text>
-    </View>
+    <SafeAreaView style={styles.safeContainer}>
+      <View style={styles.container}>
+        <HomeHeaderView style={styles.header} />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeContainer: {
+    flex: 1,
+    backgroundColor: COLORS.background
+  },
   container: {
     flex: 1,
-    backgroundColor: "green",
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 30
   },
+  header: {
+    flex: 1
+  }
 });
 
 export default HomeScreen;
