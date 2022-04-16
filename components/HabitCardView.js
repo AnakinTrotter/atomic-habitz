0ae1971c5d4e62a78c3eb25d6684b62e40eeccbf
Eugene Lo <bleugene12@g.ucla.edu>
2022-04-16 16:02:45 -0700

    Added collapsing and expanding

--- components/HabitCardView.js
+++ components/HabitCardView.js
@@ -4,23 +4,35 @@ import {
   View,
   TextInput,
   TouchableOpacity,
+  TouchableWithoutFeedback,
   useWindowDimensions
 } from 'react-native';
+import { useState } from 'react';
 import { COLORS } from '../constants/colors';
+import RCTSingelineTextInputNativeComponent from 'react-native/Libraries/Components/TextInput/RCTSingelineTextInputNativeComponent';
 
 function HabitStackView(props) {
-  createHabitStack = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const tapHandler = () => {
+    console.log("OAEIHFAOIWHFOIAHOIFAHOGI");
+    setExpanded(!expanded);
+  }
+
+  const createHabitStack = () => {
     return props.habits.map((h, i) => {
       const isBackground = i !== 0;
 
-      return <HabitCardView key={i} isBackground={isBackground} id={i} zIndex={props.habits.length - i} />
+      return <HabitCardView expanded={expanded} key={i} isBackground={isBackground} id={i} zIndex={props.habits.length - i} />
     })
   }
 
   return (
-    <View style={styles.container}>
-      {createHabitStack()}
-    </View>
+    <TouchableWithoutFeedback onPress={tapHandler} >
+      <View style={{...styles.container, marginLeft: expanded ? 0 : 45}}>
+        {createHabitStack()}
+      </View>
+    </TouchableWithoutFeedback>
   )
 }
 
@@ -31,14 +43,23 @@ HabitStackView.defaultProps = {
 
 function HabitCardView(props) {
   const window = useWindowDimensions();
+  const cardStyle = !props.expanded ? {
+    opacity: 1 - (props.id * 0.10),
+    top: props.id * 5,
+    left: props.id * 5 - (window.width / 2),
+  } : {
+    opacity: 1,
+    left: -(styles.card.width / 2),
+    top: props.id * styles.card.height + (props.id * 5),
+  }
+  
   
   return (
     <View style={{...styles.card, 
       backgroundColor: props.isBackground ? COLORS.primary : "#F6F6F6",
       zIndex: props.zIndex,
-      top: props.id * 5,
-      left: props.id * 5 - (window.width / 2),
-      opacity: 1 - (props.id * 0.20)
+      position: "absolute",
+      ...cardStyle
     }} />
   )
 }
@@ -56,7 +77,6 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.2,
     shadowRadius: 3,
-    position: "absolute",
   },
 });
 
