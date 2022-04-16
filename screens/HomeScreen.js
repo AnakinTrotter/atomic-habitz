3c1204fd256bb25bad35dae72977a00d7d59d60d
jamesmwu <94282326+jamesmwu@users.noreply.github.com>
2022-04-16 14:21:56 -0700

    Made a button

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,8 +6,8 @@ import {
   TouchableOpacity,
 } from "react-native";
 
-import HomeHeaderView from '../components/HomeHeaderView.js'
-import { COLORS } from '../constants/colors'
+import HomeHeaderView from "../components/HomeHeaderView.js";
+import { COLORS } from "../constants/colors";
 
 function HomeScreen(props) {
   // the HomeHeaderView is for testing rn
@@ -16,6 +16,12 @@ function HomeScreen(props) {
       <View style={styles.container}>
         <HomeHeaderView style={styles.header} />
       </View>
+      <TouchableOpacity
+        style={styles.add}
+        onPress={() => {
+          console.log("Button pressed");
+        }}
+      ></TouchableOpacity>
     </SafeAreaView>
   );
 }
@@ -23,15 +29,27 @@ function HomeScreen(props) {
 const styles = StyleSheet.create({
   safeContainer: {
     flex: 1,
-    backgroundColor: COLORS.background
+    backgroundColor: COLORS.background,
   },
   container: {
     flex: 1,
-    padding: 30
+    padding: 30,
   },
   header: {
-    flex: 1
-  }
+    flex: 1,
+  },
+
+  add: {
+    height: 80,
+    width: 80,
+    borderRadius: 80,
+    marginRight: 10,
+    backgroundColor: "white",
+    shadowColor: "#171717",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
 });
 
 export default HomeScreen;
