619c5592f7db57c279c838c5af8e4be876dd589c
Eugene Lo <bleugene12@g.ucla.edu>
2022-04-16 12:09:13 -0700

    Added color constants

---
+++ components/HabitCardView.js
@@ -0,0 +1,23 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import { COLORS } from './constants/colors';
+
+function HabitCardView(props) {
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "green",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default HabitCardView;
